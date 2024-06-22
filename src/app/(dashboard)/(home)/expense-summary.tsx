7a35063c53e924938b121b17a4ba347bba12d2e5
Miguel Ângelo <miguelcg958@gmail.com>
2024-06-22 17:14:55 -0300

    make expense summary production ready

--- src/app/(dashboard)/(home)/expense-summary.tsx
+++ src/app/(dashboard)/(home)/expense-summary.tsx
@@ -1,13 +1,43 @@
+'use client';
+
+import { formatters } from '@/libs/formatters';
+import { type Expense, useExpenses } from '@/stores/expenses';
+import { useMemo } from 'react';
+
 const ExpenseSummary: React.FC = () => {
+  const expenses = useExpenses((store) => store.expenses);
+  const month = new Date().toLocaleString('pt-BR', { month: '2-digit', year: 'numeric' });
+
+  const total = useMemo(() => {
+    const grouped = expenses.reduce((acc, expense) => {
+      const month = new Date(expense.date).toLocaleString('pt-BR', {
+        month: '2-digit',
+        year: 'numeric',
+      });
+
+      if (!acc[month]) {
+        acc[month] = [];
+      }
+
+      acc[month].push(expense);
+
+      return acc;
+    }, {} as Record<string, Expense[]>);
+
+    const total = grouped[month]?.reduce((acc, expense) => expense.amount + acc, 0) || 0;
+
+    return formatters.currency(total);
+  }, [expenses, month]);
+
   return (
     <section className="flex flex-col items-center justify-center mt-16">
       {/* Balance */}
 
-      <p className="text-foreground/70">Gastos este mês</p>
+      <p className="text-foreground/70">Gastos do mês</p>
       <div className="flex">
         <span className="text-4xl text-foreground/50">R$</span>
-        <span className="text-6xl">1.234</span>
-        <span className="text-4xl">,56</span>
+        <span className="text-6xl">{total.replace('R$', '').replace(/(.+)\,\d{2}$/, '$1').trim()}</span>
+        <span className="text-4xl">{total.replace('R$', '').replace(/.+(\,\d{2})$/, '$1').trim()}</span>
       </div>
     </section>
   );
--- src/stores/expenses.ts
+++ src/stores/expenses.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
-interface Expense {
+export interface Expense {
   id: string;
   description: string;
   amount: number;
